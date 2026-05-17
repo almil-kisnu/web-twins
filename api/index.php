7b78ecee46bc1c191b5c612f783fedf2c55b3a4d
almil-kisnu <hisnulloh21@gmail.com>
2026-05-17 17:09:56 +0700

    gass

--- api/index.php
+++ api/index.php
@@ -20,21 +20,6 @@ if (isset($_SERVER['VERCEL']) || isset($_ENV['VERCEL'])) {
     $_ENV['LOG_CHANNEL'] = 'stderr';
     putenv('LOG_CHANNEL=stderr');
 
-    $_ENV['APP_CONFIG_CACHE'] = '/tmp/laravel/config.php';
-    putenv('APP_CONFIG_CACHE=/tmp/laravel/config.php');
-
-    $_ENV['APP_EVENTS_CACHE'] = '/tmp/laravel/events.php';
-    putenv('APP_EVENTS_CACHE=/tmp/laravel/events.php');
-
-    $_ENV['APP_ROUTES_CACHE'] = '/tmp/laravel/routes.php';
-    putenv('APP_ROUTES_CACHE=/tmp/laravel/routes.php');
-
-    $_ENV['APP_SERVICES_CACHE'] = '/tmp/laravel/services.php';
-    putenv('APP_SERVICES_CACHE=/tmp/laravel/services.php');
-
-    $_ENV['APP_PACKAGES_CACHE'] = '/tmp/laravel/packages.php';
-    putenv('APP_PACKAGES_CACHE=/tmp/laravel/packages.php');
-    
     $_ENV['CACHE_STORE'] = 'array';
     putenv('CACHE_STORE=array');
 }
